59fd8b2af8aaddc688001b67da951fb1dd80f64d
dmitri.gorchanin <dmitri.gorchanin@MrBrown.local>
2018-02-21 18:20:18 +0300

    55%

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  var summ = [];
+  for (var i=0; i<preferences.length; i++)
+  { 
+    if ((i+1 == preferences[preferences[preferences[i]-1]-1])&&(preferences[i]!==preferences[preferences[preferences[i]-1]-1])){
+      //console.log(i,preferences[preferences[preferences[i]-1]-1]);
+      if (summ.indexOf(preferences[i]+preferences[preferences[i]-1]+preferences[preferences[preferences[i]-1]-1])==-1){
+        count++;
+      }
+      summ.push(preferences[i]+preferences[preferences[i]-1]+preferences[preferences[preferences[i]-1]-1])
+    }
+  }
+  console.log(count);
+
+  return count;
 };
